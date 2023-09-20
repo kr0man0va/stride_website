47124a8cfd3abe25c0ed446cb6ef32f4355202ed
kr0man0va <67516645+kr0man0va@users.noreply.github.com>
2023-09-20 18:08:25 -0400

    Created menu bar

---
+++ stride-app/src/components/Description.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../images/react.png';
+
+const Description = () => {
+    return(
+        <div id="description">
+            Description
+            <img src={logo} alt='React'></img>
+        </div>
+    );
+}
+
+export default Description;
---
+++ stride-app/src/components/Instructions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../images/react.png';
+
+const Instructions = () => {
+    return(
+        <div id="instructions">
+            Instructions
+            <img src={logo} alt='React'></img>
+        </div>
+    );
+}
+
+export default Instructions;
