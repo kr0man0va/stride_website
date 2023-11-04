5a77a6d3866f5e27c9b524b0f2b535db37b6b6d2
kr0man0va <67516645+kr0man0va@users.noreply.github.com>
2023-11-04 19:02:10 -0400

    Created react table
    Used example data to create a table

--- stride-app/src/components/ROI.js
+++ stride-app/src/components/ROI.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import logo from '../images/react.png';
+import React, {useState, useEffect} from 'react';
+import * as xlsx from "xlsx";
+import { MDBDataTable } from 'mdbreact';
+import exampleFile from '../context/state_M2019_dl.xlsx';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 const ROI = () => {
-    return(
+
+    const [parsedData, setParsedData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(exampleFile);
+            const arrayBuffer = await response.arrayBuffer();
+            const data = new Uint8Array(arrayBuffer);
+
+            const workbook = xlsx.read(data, { type: 'array' });
+            const sheetName = workbook.SheetNames[6];
+            const worksheet = workbook.Sheets[sheetName];
+            const jsonData = xlsx.utils.sheet_to_json(worksheet, { raw: false });
+
+            setParsedData(jsonData);
+        };
+    
+        fetchData();
+    }, []);
+
+    const columns = parsedData.length > 0 ? Object.keys(parsedData[0]).filter((key) => key.trim() !== 'Latitude' && key.trim() !== 'Longitude').map((key) => ({
+        label: key,
+        field: key,
+      })) : [];
+    
+      const data = {
+        columns: columns,
+        rows: parsedData,
+      };
+    
+      return (
         <div id="roi">
-            ROI
-            <img src={logo} alt='React'></img>
+          {parsedData.length > 0 && <MDBDataTable responsive striped bordered small data={data} paging />}
         </div>
-    );
-}
+      );
+    };
 
 export default ROI;
